126e838a47f5a770c7c4beac6ae7e04f1be25938
g-sanchez01 <g.sanchez@settepi.com>
2026-05-26 17:58:07 -0600

    update

---
+++ src/components/general/encuesta-ideas/IdeasTable.vue
@@ -0,0 +1,10 @@
+
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Tabla de Ideas</h1>
+    </div>
+</template>
--- src/views/UsersGeneral/Encuestas/TusIdeasNosMueven/HomeIdeasView.vue
+++ src/views/UsersGeneral/Encuestas/TusIdeasNosMueven/HomeIdeasView.vue
@@ -1,9 +1,12 @@
 <script setup>
     import IdeasHeader from '@/components/general/encuesta-ideas/IdeasHeader.vue';
+    import IdeasTable from '@/components/general/encuesta-ideas/IdeasTable.vue';
 </script>
 
 <template>
     <div class="p-4 lg:ml-72 lg:p-6">
         <IdeasHeader/>
+
+        <IdeasTable/>
     </div>
 </template>
