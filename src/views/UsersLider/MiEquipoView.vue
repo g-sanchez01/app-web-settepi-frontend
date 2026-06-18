f66d97e2e6f13b0cea1b4c7a0506cee240731c80
g-sanchez01 <g.sanchez@settepi.com>
2026-06-17 18:25:34 -0600

    Diseño de filtrado

--- src/views/UsersLider/MiEquipoView.vue
+++ src/views/UsersLider/MiEquipoView.vue
@@ -1,9 +1,13 @@
 <script setup>
-    import EquipoHeader from '@/components/lider/EquipoHeader.vue';
-    import TeamCard from '@/components/lider/TeamCard.vue';
-    import TeamTable from '@/components/lider/TeamTable.vue';
-    import WeekEmployeeCard from '@/components/lider/WeekEmployeeCard.vue';
-    import HistorialWeekEmployeeTable from '@/components/lider/HistorialWeekEmployeeTable.vue';
+    import EquipoHeader from '@/components/lider/miequipo/EquipoHeader.vue';
+    import TeamFilter from '@/components/lider/miequipo/TeamFilter.vue';
+    import TeamCard from '@/components/lider/miequipo/TeamCard.vue';
+    import TeamTable from '@/components/lider/miequipo/TeamTable.vue';
+    import WeekEmployeeCard from '@/components/lider/miequipo/WeekEmployeeCard.vue';
+    import HistorialWeekEmployeeTable from '@/components/lider/miequipo/HistorialWeekEmployeeTable.vue';
+
+
+    
 </script>
 
 <template>
@@ -18,13 +22,45 @@
             <WeekEmployeeCard/>
         </div>
 
+        <!--Filtros-->
+        <TeamFilter class="mb-8"
+            @filter="aplicarFiltros"
+        />
+
         <div class="mb-8">
             <TeamTable/>
         </div>
 
+        <!-- PAGINACIÓN -->
+        <!--<div
+            class="flex items-center justify-end gap-3 mt-4"
+        >
+            <button
+                @click="paginaAnterior"
+                :disabled="currentPage === 1"
+                class="px-4 py-2 border rounded-lg disabled:opacity-50 cursor-pointer"
+            >
+                Anterior
+            </button>
+
+            <span class="text-sm text-gray-600">
+                Página {{ currentPage }}
+            </span>
+
+            <button
+                @click="siguientePagina"
+                :disabled="feedbacks.length < limit"
+                class="px-4 py-2 border rounded-lg disabled:opacity-50 cursor-pointer"
+            >
+                Siguiente
+            </button>
+        </div>-->
+
         <div class="mb-8">
             <HistorialWeekEmployeeTable/>
         </div>
+
+        
         
     </div>
 </template>
