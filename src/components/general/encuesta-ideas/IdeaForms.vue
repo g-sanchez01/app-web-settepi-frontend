079b32c5a370a301453ca01dffc20697a0bd28be
g-sanchez01 <g.sanchez@settepi.com>
2026-06-30 13:22:18 -0600

    Eliminar columna zona

--- src/components/general/encuesta-ideas/IdeaForms.vue
+++ src/components/general/encuesta-ideas/IdeaForms.vue
@@ -78,23 +78,6 @@
                 }"
             />
 
-            <!-- Zona -->
-            <FormKit
-                type="select"
-                name="zona"
-                label="Zona"
-                :options="[
-                { label: 'Selecciona una zona', value: '', disabled: true },
-                { label: 'APODACA', value: 'APODACA' },
-                { label: 'ESCOBEDO', value: 'ESCOBEDO' },
-                { label: 'PESQUERIA', value: 'PESQUERIA' }
-                ]"
-                validation="required"
-                :validation-messages="{
-                    required: 'La zona es requerida',
-                }"
-            />
-
             <!-- Descripción -->
             <FormKit
                 type="textarea"
