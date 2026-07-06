bcc550d5288e58fcc4658910ec36aa4a1adfc51a
g-sanchez01 <g.sanchez@settepi.com>
2026-07-06 15:47:57 -0600

    fix expire

--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,9 @@ import router from '@/router'
 // SERVER
 const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'
 
+// Bandera para evitar múltiples alertas
+let sessionExpired = false
+
 const api = axios.create({
     baseURL: API_URL,
     headers: {
